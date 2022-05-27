b591c1940ef53dba2463d5b2268437e75404f7cb
PamLeles <pamela_leles_sousa@outlook.com>
2022-05-27 14:42:05 -0300

    counter using usecallback()

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbacksButtons from '../../components/Button/UseCallbacksButtons.jsx'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0);
+
+    const inc = useCallback((delta) => {
+        setCount(curr => curr + delta)
+    }, [setCount]);
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
+                subtitle="Retorna uma função memorizada!" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbacksButtons inc={inc} />
+            </div>
         </div>
     )
 }
