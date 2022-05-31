b7a3aae79bd7a942f89d4a8b67bb35bb33cd31ce
PamLeles <pamela_leles_sousa@outlook.com>
2022-05-31 11:50:25 -0300

    passing an object in usecontext

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -6,6 +6,12 @@ import DataContext from '../../data/DataContext'
 const UseContext = (props) => {
     const context = useContext(DataContext)
 
+    function addNumber(delta) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + delta
+        })
+    }
 
     return (
         <div className="UseContext">
@@ -13,11 +19,17 @@ const UseContext = (props) => {
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!" />
             <div className="center">
-                <span className="text">{context.text}</span>
-                <span className="text">{context.number}</span>
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+            </div>
+            <div>
+                <button className='btn'
+                    onClick={() => addNumber(-1)}>-1</button>
+                <button className='btn'
+                    onClick={() => addNumber(+1)}>+1</button>
             </div>
 
-        </div>
+        </div >
     )
 }
 
